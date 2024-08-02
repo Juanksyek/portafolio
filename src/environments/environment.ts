6683b0d568da0b0d590d9fd471a5e706a24f45af
Juan Flores <elgalleto12393@gmail.com>
2024-08-02 14:50:41 -0600

    update url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: process.env["API_URL"] || 'http://localhost:3000' // URL de tu API
+  apiUrl: 'https://sleepy-reaches-10644-af06d0d56f9b.herokuapp.com'
 };
