b0b95137b8a970a5cca95e711406a42ca49a6148
Juan Flores <elgalleto12393@gmail.com>
2026-04-22 11:18:56 -0600

    refactor: simplify header component by removing unused methods and improving scroll handling

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,45 +1,46 @@
-import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
+import { Component, Inject, OnInit, HostListener, PLATFORM_ID } from '@angular/core';
 import { isPlatformBrowser } from '@angular/common';
 import { DarkModeService } from '../../services/dark-mode.service';
 import { selected, selectNew, selectedSmall } from '../../utils';
 
 @Component({
+  standalone: false,
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  scrolled = false;
+  menuOpen = false;
+
   constructor(
     private darkModeService: DarkModeService,
     @Inject(PLATFORM_ID) private platformId: object
   ) {}
 
   ngOnInit() {
+    // visual position of the toggle is handled via Angular bindings in template
+  }
+
+  @HostListener('window:scroll')
+  onScroll() {
     if (isPlatformBrowser(this.platformId)) {
-      this.updateDotPosition();
+      this.scrolled = window.scrollY > 20;
     }
   }
+
+  toggleMenu() { this.menuOpen = !this.menuOpen; }
 
   toggleDarkMode() {
     this.darkModeService.toggleDarkMode();
-    if (isPlatformBrowser(this.platformId)) {
-      this.updateDotPosition();
-    }
   }
 
-  updateDotPosition() {
-    const dot = document.querySelector('.dot');
-    if (dot) {
-      const darkModeEnabled = this.darkModeService.isDarkModeEnabled();
-      if (darkModeEnabled) {
-        dot.classList.add('transform', 'translate-x-full');
-      } else {
-        dot.classList.remove('transform', 'translate-x-full');
-      }
-    }
+  get isDark(): boolean {
+    return this.darkModeService.isDarkModeEnabled();
   }
 
   selected = selected;
   selectNew = selectNew;
   selectedSmall = selectedSmall;
 }
+
